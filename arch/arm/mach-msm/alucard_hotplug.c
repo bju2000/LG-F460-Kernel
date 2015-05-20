8002bdadd24f6cbe63348108b5b3021b0d68393c
shminer <332574643@qq.com>
2015-05-20 18:20:35 +0800

    Revert "tweak for twrp"
    This reverts commit 939b75399f437a175c4710d72ea261ac765af615.

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -61,7 +61,11 @@ static struct hotplug_tuners {
 	bool suspended;
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 30,
+#ifdef CONFIG_MACH_JF
 	.hotplug_enable = 1,
+#else
+	.hotplug_enable = 0,
+#endif
 	.min_cpus_online = 1,
 	.maxcoreslimit = NR_CPUS,
 	.maxcoreslimit_sleep = 1,
